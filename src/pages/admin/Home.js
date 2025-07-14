e0bb880879da6905b4f1ca8c2fcaabd00cc67403
Nikitha <nik-216@users.noreply.github.com>
2025-07-14 11:00:37 +0530

    split components

--- src/pages/admin/Home.js
+++ src/pages/admin/Home.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../pages/auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import Sidebar from './components/Sidebar/Sidebar';
-import Dashboard from './components/Dashboard/Dashboard';
-import ChatbotPage from './components/ChatbotPage/ChatbotPage';
-import { ChatbotProvider } from './components/ChatbotPage/ChatbotContext';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Dashboard from '../../components/Dashboard/Dashboard';
+import ChatbotPage from '../../components/ChatbotPage/ChatbotPage';
+import { ChatbotProvider } from '../../components/ChatbotPage/ChatbotContext';
 import './Home.css';
 
 const Home = () => {
